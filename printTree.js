a939713579e65d8c625b50618c315ee8b68d86a5
lbouzon <lbouzon@dwt.com>
2018-11-15 15:38:39 -0300

    htmls and printtree

--- printTree.js
+++ printTree.js
@@ -1,2 +1,54 @@
 // JavaScript source code
-abc = "la casa de carlos";
+
+
+
+
+function treeToDic(node) {
+    let keyValue = { value: null, height: null }
+    let array  = [];
+
+    if (node.left.value !== null) {
+        array = array.concat(treeToDic(node.left));
+    }
+
+    keyValue.value = node.value;
+    keyValue.height = node.height;
+    array.push(keyValue);
+    console.log(array);
+
+    if (node.right.value !== null) {
+        array = array.concat(treeToDic(node.right));
+
+    }
+
+
+    
+    return array;
+}
+
+function addDivs(array) {
+    wood = document.getElementById("wood");
+
+    for (each in array) {
+        
+        let altura = array[each].height * 50;
+        let leftOrder = each * 100 / array.length;
+        
+        let container = document.createElement("div");
+        container.innerHTML = array[each].value;
+        container.className = "node";
+        let stringStyle = "height:" + altura + "px;";
+        stringStyle += "left:" + leftOrder + "%;";
+
+
+
+        container.style = stringStyle;
+
+        wood.appendChild(container);
+        
+        
+    }
+
+
+
+}
