678d56a3051ab2d87d03b05b141639511ae5992c
lbouzon <lbouzon@dwt.com>
2018-11-15 17:43:49 -0300

    SVG initial

--- printTree.js
+++ printTree.js
@@ -3,61 +3,62 @@
 
 
 
-function treeToDic(node) {
-    let keyValue = { value: null, height: null }
+function treeToDic(node, path = "") {
+    const keyValue = { value: null, height: null, path}
     let array  = [];
 
     if (node.left.value !== null) {
-        array = array.concat(treeToDic(node.left));
+        array = array.concat(treeToDic(node.left, path + "0"));
     }
-
     keyValue.value = node.value;
     keyValue.height = node.height;
     array.push(keyValue);
-    console.log(array);
+  
 
     if (node.right.value !== null) {
-        array = array.concat(treeToDic(node.right));
-
+        array = array.concat(treeToDic(node.right, path + "1"));
     }
-
-
-    
     return array;
 }
 
-function addDivs(array) {
-    wood = document.getElementById("wood");
-
-    for (each in array) {
-
-        let altura = array[each].height * 50;
-        let leftOrder = (each * 100 / array.length)+3;
-        let ancho = 100 / array.length -3;
-
-        let container = document.createElement("div");
-        container.innerHTML = array[each].value;
-        container.className = "node";
-        let stringStyle = "bottom:" + altura + "px;";
-        stringStyle += "left:" + leftOrder + "%;";
-
-        stringStyle += "width:" + ancho + "%;";
+    function addDivs(array,treeHeight) {
+        wood = document.getElementById("wood");
+        for (each in array) {
+            let altura = (treeHeight-array[each].path.length )* 50;
+            let leftOrder = (each * 100 / array.length)+3;
+            let ancho = 100 / array.length -3;
+            let container = document.createElement("div");
+            let stringStyle
+            container.innerHTML = array[each].value;
+            container.className = "node";
 
 
+            stringStyle = "bottom:" + altura + "px;";
+            stringStyle += "left:" + leftOrder + "%;";
+            stringStyle += "width:" + ancho + "%;";
 
-        container.style = stringStyle;
-
-        wood.appendChild(container);
-
-
+            container.style = stringStyle;
+            wood.appendChild(container);
+        }
     }
-}
+
+
     function printGraph() {
         document.getElementById("wood").innerHTML = null;
-        addDivs(treeToDic(tree));
-
-
-        
+        addDivs(treeToDic(tree),tree.height);
 
     }
 
+
+
+
+
+
+    function drawlines() {
+
+        let listNodos = document.getElementsByClassName("node");
+        for (i = 0; i < listNodos.length; i++){
+            console.log(listNodos[i].getBoundingClientRect());
+        }
+        
+    }
